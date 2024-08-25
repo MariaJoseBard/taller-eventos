3456fafe2e0218bec0da83ec9d04a833a6dcd8db
MariaJoseBard <bardesio.mariajose@gmail.com>
2024-08-25 12:52:22 -0300

    Agregue evento click al div

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+    const contenedorBoton= document.querySelector('.Contenedor-boton');
+    contenedorBoton.addEventListener('click', function(){
+        alert('Hola! Soy el div');
+    })
+})
